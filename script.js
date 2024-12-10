0a8f6471d63232fe7db722c5d838ca943b5abec4
unknown <martinarielle159@gmail.com>
2024-12-09 23:35:28 -0500

    Added css fonts and based html/js

---
+++ script.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+const codeEl = document.querySelector("code");
+console.log(codeEl);
+
+const app = document.querySelector("#app");
+console.log(app);
+
+const firstSelect = document.querySelector("input[type=checkbox]");
+console.log(firstSelect);
